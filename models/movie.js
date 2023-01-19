b1fa2a4328093e689232b850600383eaec24f5b0
Evgeniy Milyakov <milyakove@gmail.com>
2023-01-19 09:08:05 +0300

    create backend, fixed utils and middlewares

--- models/movie.js
+++ models/movie.js
@@ -2,68 +2,69 @@ const mongoose = require('mongoose');
 const isURL = require('validator/lib/isURL');
 const { WRONG_URL } = require('../utils/constants');
 
+//Создание схемы movie
 const movieSchema = new mongoose.Schema({
-    country: {
-        type: String,
-        required: true,
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: WRONG_URL,
     },
-    director: {
-        type: String,
-        required: true,
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: WRONG_URL,
     },
-    duration: {
-        type: String,
-        required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: WRONG_URL,
     },
-    year: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-        validate: {
-            validator: (v) => isURL(v),
-            message: WRONG_URL,
-        },
-    },
-    trailerLink: {
-        type: String,
-        required: true,
-        validate: {
-            validator: (v) => isURL(v),
-            message: WRONG_URL,
-        },
-    },
-    thumbnail: {
-        type: String,
-        required: true,
-        validate: {
-          validator: (v) => isURL(v),
-          message: WRONG_URL_FORMAT,
-        },
-      },
-      owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-        required: true,
-      },
-      movieId: {
-        type: Number,
-        required: true,
-      },
-      nameRU: {
-        type: String,
-        required: true,
-      },
-      nameEN: {
-        type: String,
-        required: true,
-      },
-    });
-    
-module.exports = mongoose.model('movie', movieSchema);    
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
